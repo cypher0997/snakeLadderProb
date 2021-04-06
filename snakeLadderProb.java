3462f8e531f4eb5553e367c3f206d75fb9b5fcf6
testForMe-Now <abcodehash@gmail.com>
2021-04-06 21:53:20 +0530

    UC3-->check for snake or ladder condition is added

--- snakeLadderProb.java
+++ snakeLadderProb.java
@@ -1,18 +1,34 @@
 class snakeLadderProb{
-	int startPosition;
+	int currentPosition;
 	int numberOccured;
 	double empCheck;
+	int ladder_or_snake;
 	snakeLadderProb(int x){
-	startPosition=x;
+	currentPosition=x;
 	}
 	public int rollDice(){
-		empCheck=Math.floor(Math.random()*(6-1+1)+1);
+		empCheck = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 		numberOccured = (int)empCheck;
-		return numberOccured;
+		double get = Math.floor(Math.random() * 10) % 2;
+		ladder_or_snake = (int)get;
+		System.out.println("the ladder_or_snake is "+ladder_or_snake);
+		System.out.println("the currentPosition is "+currentPosition);
+		if (ladder_or_snake == 0){
+			currentPosition = currentPosition+numberOccured;
+		}
+		else if(ladder_or_snake == 1 && currentPosition == 0)
+		{
+			currentPosition=0;
+		}
+		else if(ladder_or_snake == 1)
+		{
+			currentPosition=currentPosition-numberOccured;
+		}
+		return currentPosition;
 	}
 	public static void main (String args[]){
 		snakeLadderProb object = new snakeLadderProb(0);
 		int show=object.rollDice();
-		System.out.println("the startPosition is "+show);
+		System.out.println("the updates currentPosition is "+show);
 	}
 }
