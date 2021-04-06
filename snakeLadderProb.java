ba264e8b83bf9634613d7d9eec7ec47c78ba2782
testForMe-Now <abcodehash@gmail.com>
2021-04-06 21:51:20 +0530

    UC2--> to get random number between 1 to 6 is added

--- snakeLadderProb.java
+++ snakeLadderProb.java
@@ -1,11 +1,18 @@
 class snakeLadderProb{
 	int startPosition;
+	int numberOccured;
+	double empCheck;
 	snakeLadderProb(int x){
 	startPosition=x;
 	}
+	public int rollDice(){
+		empCheck=Math.floor(Math.random()*(6-1+1)+1);
+		numberOccured = (int)empCheck;
+		return numberOccured;
+	}
 	public static void main (String args[]){
 		snakeLadderProb object = new snakeLadderProb(0);
-		int show=object.startPosition;
+		int show=object.rollDice();
 		System.out.println("the startPosition is "+show);
 	}
 }
