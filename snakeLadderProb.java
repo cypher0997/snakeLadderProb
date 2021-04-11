7dfddd9d9a45fadc8655c0f5733bbca47a3f9905
testForMe-Now <abcodeHash@gmail.com>
2021-04-11 10:21:59 +0530

    game with player 2 added

--- snakeLadderProb.java
+++ snakeLadderProb.java
@@ -1,57 +1,110 @@
 class snakeLadderProb{
-	int diceRollCount,currentPosition,numberOccured,ladder_or_snake,stable=0;
-	double empCheck;
+	int diceRollCountPlayerOne=0,diceRollCountPlayerTwo=0,numberOccured,ladder_or_snake,stable=0,playerOne=0,playerTwo=0,turn=0;
+	double numOccured;
 	snakeLadderProb(int x){
-	currentPosition=x;
-	diceRollCount=x;
+	playerOne=x;
+	playerTwo=x;
+	diceRollCountPlayerOne=x;
+	diceRollCountPlayerTwo=x;
 	}
-	public int rollDice(){
+	public int rollDice()
+	{
 		while(true)
 		{
-			diceRollCount++;
-			empCheck=Math.floor(Math.random()*(6-1+1)+1);
-			numberOccured = (int)empCheck;
-			double get=(int)Math.floor(Math.random()*10)%2;
-			ladder_or_snake=(int)get;
-			System.out.println("the ladder_or_snake is "+ladder_or_snake);
-			System.out.println("the currentPosition is "+currentPosition);
-			if (ladder_or_snake == 0 && currentPosition < 100)
-			{
-				stable=currentPosition;
-				currentPosition = currentPosition+numberOccured;
-				if(currentPosition > 100)
+			turn = (int)Math.floor(Math.random()*10)%2;
+			if(turn == 0)
+			{	
+					System.out.println("rightnow control is in one");
+					diceRollCountPlayerOne++;
+					numOccured = Math.floor(Math.random()*(6-1+1)+1);
+					numberOccured = (int)numOccured;
+					double get = Math.floor(Math.random()*10)%2;
+					ladder_or_snake=(int)get;
+					System.out.println("the ladder_or_snake is "+ ladder_or_snake);
+					System.out.println("the currentPosition is "+ playerTwo);
+					if (ladder_or_snake == 0 && playerOne < 100)
+					{
+						stable=playerOne;
+						playerOne = playerOne + numberOccured;
+						if(playerOne > 100)
+						{
+							playerOne=stable;
+						}
+					}
+					else if(ladder_or_snake == 1 && playerOne == 0 )
+					{
+						playerOne=0;
+					}
+					else if(ladder_or_snake == 1 && playerOne < 0.0)
+					{
+						playerOne=0;
+					}
+					else if(ladder_or_snake == 1)
+					{
+						playerOne = playerOne - numberOccured;
+						if(playerOne < 0.0)
+						{
+							playerOne = 0;
+						}
+					}
+			}
+			if(playerOne == 100)
 				{
-					currentPosition=stable;
+					return playerOne;
 				}
-			}
-			else if(ladder_or_snake == 1 && currentPosition == 0 )
+			if(turn == 1)
 			{
-				currentPosition=0;
+					System.out.println("rightnow control is in two");
+					diceRollCountPlayerTwo++;
+					numOccured = Math.floor(Math.random()*(6-1+1)+1);
+					numberOccured = (int)numOccured;
+					double get = Math.floor(Math.random()*10)%2;
+					ladder_or_snake=(int)get;
+					System.out.println("the ladder_or_snake is "+ ladder_or_snake);
+					System.out.println("the currentPosition is "+ playerTwo);
+					if (ladder_or_snake == 0 && playerTwo < 100)
+					{
+						stable = playerTwo;
+						playerTwo = playerTwo + numberOccured;
+						if(playerTwo > 100)
+						{
+							playerTwo = stable;
+						}
+					}
+					else if(ladder_or_snake == 1 && playerTwo == 0 )
+					{
+						playerTwo = 0;
+					}
+					else if(ladder_or_snake == 1 && playerTwo < 0.0)
+					{
+						playerTwo = 0;
+					}
+					else if(ladder_or_snake == 1)
+					{
+						playerTwo = playerTwo - numberOccured;
+						if(playerTwo < 0.0)
+						{
+							playerTwo = 0;
+						}
+					}
 			}
-			else if(ladder_or_snake == 1 && currentPosition < 0.0)
+			if(playerTwo == 100)
 			{
-				currentPosition=0;
+				return playerTwo;
 			}
-			else if(ladder_or_snake == 1)
-			{
-				currentPosition=currentPosition-numberOccured;
-				if(currentPosition < 0.0)
-				{
-					currentPosition=0;
-				}
-			}
-			if(currentPosition == 100)
-			{
-				break;
-			}
+		if(playerOne == 100 || playerTwo == 100)
+			break;
 		}
-		return currentPosition;
+		return 0;
 	}
+
 	
 	public static void main (String args[]){
 		snakeLadderProb object = new snakeLadderProb(0);
-		int show=object.rollDice();
-		System.out.println("the updates currentPosition is "+show);
-		System.out.println("the total number of time dice rolled is "+object.diceRollCount);
+		object.rollDice();
+		if(object.diceRollCountPlayerOne < object.diceRollCountPlayerTwo)
+			System.out.println("playerOne wins");
+		else
+			System.out.println("playerTwo wins");
 	}
 }
